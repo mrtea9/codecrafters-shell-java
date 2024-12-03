ed4e56a852e3ffdc29a3b0bc4369f1ed69042d66
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 11:35:41 +0200

    type builtin

--- src/main/java/store/Storage.java
+++ src/main/java/store/Storage.java
@@ -1,18 +1,14 @@
 package store;
 
 import command.Command;
-import command.builtin.EchoCommand;
-import command.builtin.ExitCommand;
-import command.builtin.TypeCommand;
 
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.function.BiFunction;
 
 public class Storage {
 
-    private Map<String, BiFunction<String, List<String>, Command>> parsers;
+    private final Map<String, BiFunction<String, List<String>, Command>> parsers;
 
     public Storage(Map<String, BiFunction<String, List<String>, Command>> parsers) {
         this.parsers = parsers;
