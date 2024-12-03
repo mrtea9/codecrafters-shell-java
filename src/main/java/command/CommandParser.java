e7b7881501625e0960187f784358c31202622c6e
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 11:54:09 +0200

    type builtin

--- src/main/java/command/CommandParser.java
+++ src/main/java/command/CommandParser.java
@@ -31,6 +31,8 @@ public class CommandParser {
     public ParsedCommand parse(String input) {
         if (input.isEmpty()) throw new IllegalStateException("Input is empty");
 
+        System.out.println(System.getenv("PATH"));
+
         List<String> arguments = new ArrayList<>(Arrays.asList(input.split(" ", 2)));
 
         String name = arguments.getFirst();
