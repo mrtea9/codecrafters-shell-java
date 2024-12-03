7937c69956a9aad289b601ee5a5283424fa4d373
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 15:20:30 +0200

    pwd command

--- src/main/java/command/CommandParser.java
+++ src/main/java/command/CommandParser.java
@@ -39,7 +39,6 @@ public class CommandParser {
 
         final var command = parser.apply(name, arguments.subList(1, arguments.size()));
 
-        System.out.println("sad");
         return new ParsedCommand(arguments, command);
     }
 
