cd71580fa83fc39d4c24ffd3e908a73a5cce5e3d
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 16:16:13 +0200

    cd absolute path

--- src/main/java/command/builtin/CdCommand.java
+++ src/main/java/command/builtin/CdCommand.java
@@ -12,7 +12,7 @@ public record CdCommand(String directoryName) implements Command {
     public CommandResponse execute(Storage storage) {
         File directory = new File(directoryName);
 
-        if (!directory.exists()) return new CommandResponse("cd: <%s>: No such file or directory".formatted(directory);
+        if (!directory.exists()) return new CommandResponse("cd: <%s>: No such file or directory".formatted(directoryName));
 
         System.setProperty("user.dir", directoryName);
 
