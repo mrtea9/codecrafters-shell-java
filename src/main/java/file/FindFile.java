d5298eee9454b41900ac9742acff064a4fe53b7e
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 12:14:45 +0200

    type builtin

--- src/main/java/file/FindFile.java
+++ src/main/java/file/FindFile.java
@@ -15,6 +15,7 @@ public class FindFile {
         for (String s : pathArray) {
             File file = new File(s);
             File[] list = file.listFiles();
+            if (list == null) continue;
             for (var file1 : list) {
                 result.add(file1.getName());
             }
