f975ee6618a447e597bdf4487ca6f2bc10b75889
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 13:55:22 +0200

    type builtin

--- src/main/java/command/builtin/TypeCommand.java
+++ src/main/java/command/builtin/TypeCommand.java
@@ -11,6 +11,8 @@ public record TypeCommand(String command) implements Command {
         final var value = storage.getParsers().get(command);
         final var executable = storage.getExecutables().get(command);
 
+        System.out.println(executable);
+
         if (value != null && executable != null) return new CommandResponse("%s is a shell builtin".formatted(command));
 
         if (executable != null) return new CommandResponse("%s is %s".formatted(command, executable));
