8810c5f60073a2967869fa085fda9812cc470d12
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 17:02:19 +0200

    cd relative path

--- src/main/java/command/builtin/CdCommand.java
+++ src/main/java/command/builtin/CdCommand.java
@@ -10,16 +10,22 @@ public record CdCommand(String directoryName) implements Command {
 
     @Override
     public CommandResponse execute(Storage storage) {
-        if (directoryName.startsWith("./")) {
-            System.out.println(directoryName);
-        }
+        String directoryInter = directoryName;
 
-        File directory = new File(directoryName);
+        if (directoryInter.startsWith("./")) directoryInter = nextDirectory();
 
-        if (!directory.exists()) return new CommandResponse("cd: %s: No such file or directory".formatted(directoryName));
+        System.out.println(directoryInter);
 
-        System.setProperty("user.dir", directoryName);
+        File directory = new File(directoryInter);
+
+        if (!directory.exists()) return new CommandResponse("cd: %s: No such file or directory".formatted(directoryInter));
+
+        System.setProperty("user.dir", directoryInter);
 
         return null;
+    }
+
+    private String nextDirectory() {
+        return "%s/%s".formatted(System.getProperty("user.dir"), directoryName.substring(2));
     }
 }
