ded9fa4ae16ef8d0ec6d0c04311ca834acd62c45
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 12:10:26 +0200

    type builtin

--- src/main/java/file/FindFile.java
+++ src/main/java/file/FindFile.java
@@ -1,18 +1,25 @@
 package file;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FindFile {
 
     private final String[] pathArray = System.getenv("PATH").split(":");
 
     public void findFile() {
-        File file = new File(pathArray[0]);
-        File[] list = file.listFiles();
-        for (var file1 : list) {
-            System.out.println(file1.getName());
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < pathArray.length; i++) {
+            File file = new File(pathArray[i]);
+            File[] list = file.listFiles();
+            for (var file1 : list) {
+                result.add(file1.getName());
+            }
         }
+
+        System.out.println(result);
     }
 
 }
