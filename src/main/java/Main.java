7120f59b3a20fd289600a334573b86a5ba28d884
mrtea9 <ionmeriacri111@gmail.com>
2024-12-03 09:39:52 +0200

    exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Repl repl = new Repl();
 
-        while (true) {
-            System.out.print("$ ");
-            String input = scanner.nextLine();
-            System.out.println(input + ": command not found");
-        }
+        repl.startLoop();
+
     }
 }
--- src/main/java/Repl.java
+++ src/main/java/Repl.java
@@ -1,6 +1,16 @@
-
+import java.util.Scanner;
 
 public class Repl {
 
+    private Scanner scanner = new Scanner(System.in);
 
+    public void startLoop() {
+
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
+
+    }
 }
