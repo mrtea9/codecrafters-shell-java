70f843fbea52394e07362586292bbd5f5c686f5d
mrtea9 <ionmeriacri111@gmail.com>
2024-12-04 11:46:34 +0200

    single quotes

--- src/main/java/command/CommandParser.java
+++ src/main/java/command/CommandParser.java
@@ -42,10 +42,10 @@ public class CommandParser {
     }
 
     private void executeProcess(String executable, List<String> argumentsRaw) {
-        String[] arguments = argumentsRaw.getFirst().split("(?<=')\\s+(?=')");
+        String[] arguments = argumentsRaw.getFirst().split("(?<=\")\\s+(?=\")");
 
         for (int i = 0; i < arguments.length; i++) {
-            arguments[i] = arguments[i].replace("'", "").trim();
+            arguments[i] = arguments[i].replace("\"", "").trim();
         }
 
         try {
