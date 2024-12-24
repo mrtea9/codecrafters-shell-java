aff1d07b3639f40db970a08b4312bcd0276c434d
mrtea9 <ionmeriacri111@gmail.com>
2024-12-24 13:10:25 +0200

    redirect stdout

--- src/main/java/command/builtin/TypeCommand.java
+++ src/main/java/command/builtin/TypeCommand.java
@@ -13,11 +13,8 @@ public record TypeCommand() implements Command {
         final var command = arguments.get(1);
 
         final var value = storage.getParsers().get(command);
-        final var executable = storage.getExecutables().get(command);
 
         if (value != null) return new CommandResponse("%s is a shell builtin".formatted(command));
-
-        if (executable != null) return new CommandResponse("%s is %s".formatted(command, executable));
 
         return new CommandResponse("%s: not found".formatted(command));
     }
