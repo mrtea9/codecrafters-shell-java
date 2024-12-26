c9e72fa2e3499ca03e75908e609a0d2fe7e25022
mrtea9 <ionmeriacri111@gmail.com>
2024-12-26 12:33:51 +0200

    redirect stdout

--- src/main/java/command/Executable.java
+++ src/main/java/command/Executable.java
@@ -1,5 +1,8 @@
 package command;
 
+import io.RedirectStream;
+import io.RedirectStreams;
+import io.StandardNamedStream;
 import parse.Redirect;
 import store.Storage;
 
@@ -27,19 +30,14 @@ public record Executable(Path path) implements Command {
             //System.out.println("command arguments = " + commandArguments);
             //System.out.println("redirect = " + redirects.get(0));
 
+            final var redirectStreams = RedirectStreams.from(redirects);
+
             final var builder = new ProcessBuilder(commandArguments)
                     .inheritIO()
                     .directory(workingDirectory.toFile());
 
-
-            if (!redirects.isEmpty()) {
-                final var redirect = ProcessBuilder.Redirect.to(redirects.get(0).path().toFile());
-
-                builder.redirectOutput(redirect);
-
-                builder.redirectErrorStream(true);
-                builder.redirectError(redirect);
-            }
+            applyRedirect(builder, redirectStreams.output(), StandardNamedStream.OUTPUT);
+            applyRedirect(builder, redirectStreams.error(), StandardNamedStream.ERROR);
 
             final var process = builder.start();
 
@@ -51,5 +49,27 @@ public record Executable(Path path) implements Command {
         }
 
         return null;
+    }
+
+    private void applyRedirect(ProcessBuilder builder, RedirectStream stream, StandardNamedStream streamName) {
+        final var isStderr = StandardNamedStream.ERROR.equals(streamName);
+
+        switch (stream) {
+            case RedirectStream.Standard standard -> {
+                if (isStderr && StandardNamedStream.OUTPUT.equals(standard.name())) builder.redirectErrorStream(true);
+            }
+
+            case RedirectStream.File file -> {
+                file.close();
+
+                final var redirect = ProcessBuilder.Redirect.to(file.path().toFile());
+
+                if (isStderr) {
+                    builder.redirectError(redirect);
+                } else {
+                    builder.redirectOutput(redirect);
+                }
+            }
+        }
     }
 }
